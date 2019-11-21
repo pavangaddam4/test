8d6eb41e8409bfefa4a84eb51ea2e1bca7f70333
Gaddam <GAD2682@corp.stateauto.com>
2019-11-20 21:46:07 -0500

    Controller

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("services")
+public class DemoController {
+    @GetMapping(path = "/hello")
+    public String getHello() {
+        return "Hello";
+    }
+}
