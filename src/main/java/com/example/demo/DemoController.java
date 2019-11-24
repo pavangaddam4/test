c3898e9a5252c974273e4d28c0fb2ec019e7778e
pavangaddam4 <Guru@8230>
2019-11-24 11:25:14 -0500

    test

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("services")
 public class DemoController {
+    @Value("${param_name}")
+    String param_name;
+
+    @Value("${param.name}")
+    String paramName;
+
     @GetMapping(path = "/hello")
     public String getHello() {
-        return "Hello Demo1 success push update";
+        return "Hello Demo1 success push update. param_name:"+ param_name + " | param.name="+paramName ;
     }
 }
