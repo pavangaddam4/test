d0c0d25a372e29f081e4bbb17ba56d5476b00d68
pavangaddam4 <Guru@8230>
2019-11-24 13:41:21 -0500

    test

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -17,6 +17,6 @@ public class DemoController {
 
     @GetMapping(path = "/hello")
     public String getHello() {
-        return "Hello Demo1 success push update. param_name:"+ param_name + " | param.name="+paramName ;
+        return "Hello Demo1 success push update - 1. param_name:"+ param_name + " | param.name="+paramName ;
     }
 }
