67e609143c92c30336c3c7fd24e5e1b9b8fa5c81
pavangaddam4 <Guru@8230>
2019-12-03 22:32:48 -0500

    update

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -15,6 +15,6 @@ public class DemoController {
 
     @GetMapping(path = "/hello")
     public String getHello() {
-        return "Demo using jenkins. Version  4. | param.name="+paramName ;
+        return "Demo using jenkins. Version  5. | param.name="+paramName ;
     }
 }
